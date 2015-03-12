ebce47e6eae9d5320d411eacdadd05f57861851c
gehernandez1 <gehernandez1@broncs.utpa.edu>
2015-03-11 21:53:31 -0500

    Third commit, simple program structure, still a lot to be done

--- helperFunctions.h
+++ helperFunctions.h
@@ -1,1 +1,22 @@
 
+#include <stdio.h>
+#include <stdlib.h>
+
+//Chech if command is an excecutable file
+//in one of the directories specified by path
+//returns the full path name to the file
+char* lookupPath(char **,char **);
+
+//parses the command by taking the line and tokenizing into
+//array of character strings, returns one if successfull
+int parseCommand(char *,struct command_t *);
+
+//parses the PATH variable before it begins reading
+//command lines.
+int parsePath(char **);
+
+//prints prompt to screen
+void printPrompt();
+
+//reads the command inputed into the prompt
+void readCommand(char *);
--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "helperFunctions.h"
 
 int main()
 {
